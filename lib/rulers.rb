ec7fa6662128a394e7d5475b5b33603618b09a0a
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2016-09-26 08:27:52 +0500

    Chapter 2: It Almost Worked!

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,6 +4,7 @@ require "rulers/routing"
 module Rulers
   class Application
     def call(env)
+      return [404, {'Content-Type' => 'text/html'}, []] if env['PATH_INFO'] == '/favicon.ico'
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
