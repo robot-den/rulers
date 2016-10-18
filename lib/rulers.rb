700944bfc4dbf42ada9f2ef40f0d7768bdceb637
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2016-10-18 10:06:29 +0500

    Chapter 5: Where Do New Quotes Come From?

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,7 +3,6 @@ require "rulers/routing"
 require "rulers/util"
 require "rulers/dependencies"
 require "rulers/controller"
-require "rulers/file_model"
 
 module Rulers
   class Application
--- lib/rulers/file_model.rb
+++ lib/rulers/file_model.rb
@@ -5,8 +5,7 @@ module Rulers
     class FileModel
       def initialize(filename)
         @filename = filename
-        basename = File.split(filename)[-1]
-        @id = File.basename(basename, '.json').to_i
+        @id = File.basename(filename, '.json').to_i
         obj = File.read(filename)
         @hash = MultiJson.load(obj)
       end
@@ -25,6 +24,32 @@ module Rulers
         rescue
           nil
         end
+      end
+
+      def self.all
+        files = Dir["db/quotes/*.json"]
+        files.map { |f| FileModel.new f }
+      end
+
+      def self.create(attrs)
+        hash = {}
+        hash[:submitter] = attrs["submitter"] || ""
+        hash[:quote] = attrs["quote"] || ""
+        hash[:attribution] = attrs["attribution"] || ""
+
+        files = Dir["db/quotes/*.json"]
+        max_id = files.map { |f| File.basename(f, '.json').to_i }.max
+        new_file = "db/quotes/#{max_id + 1}.json"
+        File.open(new_file, 'w') do |f|
+          f.write <<TEMPLATE
+{
+  "submitter": "#{hash[:submitter]}",
+  "quote": "#{hash[:quote]}",
+  "attribution": "#{hash[:attribution]}"
+}
+TEMPLATE
+        end
+        FileModel.new(new_file)
       end
     end
   end
