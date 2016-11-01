14a2ef0cc7fc467801c2071d54420412b613f295
Denis Nazmutdinov <w.nazmutdinov@gmail.com>
2016-11-01 09:38:21 +0500

    Chapter 7: Schema and Data

--- lib/rulers/sqlite_model.rb
+++ lib/rulers/sqlite_model.rb
@@ -6,6 +6,43 @@ DB = SQLite3::Database.new 'db/database.db'
 module Rulers
   module Model
     class SQLite3
+      def initialize(data = nil)
+        @hash = data
+      end
+
+      def self.to_sql(val)
+        case val
+        when Numeric
+          val.to_s
+        when String
+          "'#{val}'"
+        else
+          raise "Can't change #{val.class} to SQL!"
+        end
+      end
+
+      def self.create(values)
+        values.delete 'id'
+        keys = schema.keys - ['id']
+        vals = keys.map do |key|
+          values[key] ? to_sql(values[key]) : 'null'
+        end
+        DB.execute <<SQL
+INSERT INTO #{table} (#{keys.join(',')})
+  VALUES (#{vals.join(',')});
+SQL
+        data = Hash[keys.zip vals]
+        sql = "SELECT last_insert_rowid();"
+        data['id'] = DB.execute(sql)[0][0]
+        self.new data
+      end
+
+      def self.count
+        DB.execute(<<SQL)[0][0]
+SELECT COUNT(*) FROM #{table}
+SQL
+      end
+      
       def self.table
         Rulers.to_underscore name
       end
